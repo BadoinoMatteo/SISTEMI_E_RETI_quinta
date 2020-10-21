0459275184d5bbf47ce1b9c20d2d3c270b9e8c96
Badoino Matteo <48619446+BadoinoMatteo@users.noreply.github.com>
2020-10-21 18:53:28 +0200

    cifrario di Vernam

---
+++ cittografia/es1.py
@@ -0,0 +1,58 @@
+def main():
+    cifratura()
+
+def cifratura():
+    # dizionario alfabeto
+    dict = {
+        'A': 0,
+        'B': 1,
+        'C': 2,
+        'D': 3,
+        'E': 4,
+        'F': 5,
+        'G': 6,
+        'H': 7,
+        'I': 8,
+        'L': 9,
+        'M': 10,
+        'N': 11,
+        'O': 12,
+        'P': 13,
+        'Q': 14,
+        'R': 15,
+        'S': 16,
+        'T': 17,
+        'U': 18,
+        'V': 19,
+        'Z': 20
+    }
+    #dichiarazione variabili
+    chiave="ITIDELPOZZO"
+    messaggio="CIAO"
+    chiaveCifrata=[]
+    messaggioCifrato=[]
+    sommaMexChiave= []
+    offset=[]
+
+    #cifratura chiave
+    for c in chiave:
+        chiaveCifrata.append(str(dict.get(c)))
+    print(chiaveCifrata)
+
+    #cifratura messaggio
+    for i in messaggio:
+        messaggioCifrato.append(str(dict.get(i)))
+    print(messaggioCifrato)
+
+    #somma messaggio e chiave
+    for a in range(0,len(messaggioCifrato)):
+        sommaMexChiave.append(str(int(chiaveCifrata[a])+ int(messaggioCifrato[a]) % 21))
+        if(int(sommaMexChiave[a])>21):
+            offset.append(1)
+        else:
+            offset.append(0)
+    print(offset)
+    print(sommaMexChiave)
+
+if __name__ == '__main__':
+    main()
