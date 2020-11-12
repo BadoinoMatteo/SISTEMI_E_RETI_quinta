3512db08be7ec194f577cb2d2b7b9c096714f240
Badoino Matteo <48619446+BadoinoMatteo@users.noreply.github.com>
2020-11-12 08:34:24 +0100

    modifica codice

--- cittografia/algoritmoEuclide.py
+++ cittografia/algoritmoEuclide.py
@@ -1,12 +1,17 @@
 
 def main():
-    numA = int(input("inserisci primo numero"))
-    numB = int(input("inserisci primo numero"))
+    corr=False
+    numA=0
+    numB=0
+    while corr==False:
+        numA = int(input("inserisci primo numero"))
+        numB = int(input("inserisci secondo numero"))
+        if(numA>numB):
+            corr=True
     while numB>0:
         resto=numA%numB
         numA=numB
         numB=resto
-    print(numA)
     print(f"mcd{numA}")
 
 if __name__ == '__main__':
--- cittografia/scomposizioneFattoriPrimi.py
+++ cittografia/scomposizioneFattoriPrimi.py
@@ -2,16 +2,27 @@ def main():
     div=[]
     num=int(input("inserisci numero da scomporre"))
     numdiv=2
-    while num>1:
-        print("ok")
-        if num%numdiv==0:
-            num=num/numdiv
-            div.append(numdiv)
-        else:
-            numdiv+=1
-    print(div)
+    if(primo(num)):
+        print("numero primo")
+    else:
+        while num>1:
+            if num%numdiv==0:
+                num=num/numdiv
+                div.append(numdiv)
+            else:
+                numdiv+=1
+        print(div)
 
-
+def primo(n):
+    div, check = 2, 0
+    while n / 2 >= div and check == 0:
+        if n % div == 0:
+            check += 1
+        div += 1
+    if (check == 0):
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     main()
